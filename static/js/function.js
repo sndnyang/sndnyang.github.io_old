de52af7bc66597f2650fc1ce6cf79c2caadf9dc9
sndnyang <sndnyangd@gmail.com>
2017-04-20 13:52:05 +0800

    超级GPA计算器

--- static/js/function.js
+++ static/js/function.js
@@ -5,14 +5,13 @@
 function calit()
 {
     var content = $("#grade").val();
-    var chart = {};
-
+    var algorithm = $("#algorithm").val();
     var elements = new Array("课程名", "百分制", "等级制", "4分制", "学分", "学时");
     var ele_eng = new Array("name", "hund", "lett", "four", "cred", "hour");
 
     // 分隔符类型
     var type = $("#type").val();
-    var map = {'csv':',', 'space':' ', ',':',', ' ':' ', '\\t':'\t', '':' '};
+    var map = {'csv':',', 'space':' ', ',':',', ' ':' ', '\\t':'\t', 'tab':'\t', '':' '};
     if (map[type]) 
     {
         type = map[type];
@@ -52,6 +51,7 @@ function calit()
         else
         {
             line = line.replace(/\s+/g, " ");
+            type = " ";
         }
         
         if (line == '' || line == ' ') continue;
@@ -73,18 +73,19 @@ function calit()
                 if (!sgrade['hund'])
                     continue;
                 //  有百分制， 则计算 ABCD
-                text = gpa_letter(sgrade['hund'], chart);
+                text = gpa_letter(sgrade['hund'], algorithm);
             }
             else if (j == 3) {
                 if (!sgrade['hund'] && !sgrade['lett']) {
                     alert(line + '  百分制 等级制 4分制 三种分数你好歹选一种吧!');
                     return 1;
                 }
-                text = gpa_four(sgrade['hund'], sgrade['lett'], chart);
+                text = gpa_four(sgrade['hund'], sgrade['lett'], algorithm);
             }
             else {
-                text = "";
+                text = ""
             }
+            // console.log(j + ' ' + text)
 
             sgrade[ele_eng[j]] = text;
             var cell = $('<td>' + text + '</td>');
@@ -103,13 +104,15 @@ function calit()
     }
 }
 
-function gpa_four(hgrade, lgrade, chart)
+function gpa_four(hgrade, lgrade, type)
 {
-    var threshold = new Array(90,85,81,78,75,72,68,64,60);
-    var letter_val = new Array('A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D');
-    var gpa_val = new Array(4.0,3.7,3.3,3.0,2.7,2.3,2.0,1.7,1.0);
+    var map = {'s4': standard_4(), 's41': standard_4_1(), 's42': standard_4_2(), 'p4': peking_4(), 'c4': canada_4_3(), 'sjt4': SJTU_4_3(), 'cs4': CSU_4_3()};
+    var calculator = map[type];
+    var threshold = calculator['threshold'];
+    var letter_val = calculator['letter'];
+    var gpa_val = calculator['gpa'];
 
-    var result = 1.0;
+    var result = 0.0;
     hgrade = hgrade - 0;
     for (var i in threshold) {
         if (!hgrade && lgrade && lgrade == letter_val[i]) {
@@ -124,11 +127,13 @@ function gpa_four(hgrade, lgrade, chart)
     return result;
 }
 
-function gpa_letter(hgrade, chart)
+function gpa_letter(hgrade, type)
 {
-    //var threshold = new Array(90,85,81,78,75,72,68,64,60);
-    var threshold = new Array(4.0,3.7,3.3,3.0,2.7,2.3,2,1.5,60);
-    var letter_val = new Array('A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D');
+    var map = {'s4': standard_4(), 's41': standard_4_1(), 's42': standard_4_2(), 'p4': peking_4(), 'c4': canada_4_3(), 'sjt4': SJTU_4_3(), 'cs4': CSU_4_3()};
+    var calculator = map[type];
+    var threshold = calculator['threshold'];
+    var letter_val = calculator['letter'];
+    var gpa_val = calculator['gpa'];
     var result = 'F';
     hgrade = hgrade - 0;
     for (var i in threshold) {
@@ -159,4 +164,60 @@ function calculator(record)
 
     return new Array(sum_credit, sum_weight_credit, 
         sum_weight_credit * 1.0 / sum_credit, sum_weight_score * 1.0 / sum_credit);
+}
+
+function standard_4() {
+    var threshold = new Array(90,80,70,60,0);
+    var letter_val = new Array('A', 'B', 'C', 'D', 'F');
+    var gpa_val = new Array(4.0,3.0,2.0,1.0,0.0);
+    var map = {'threshold': threshold, 'letter': letter_val, 'gpa': gpa_val};
+    return map;
+}
+
+function standard_4_1() {
+    var threshold = new Array(85,70,60,0);
+    var letter_val = new Array('A', 'B', 'C', 'F');
+    var gpa_val = new Array(4.0,3.0,2.0,0.0);
+    var map = {'threshold': threshold, 'letter': letter_val, 'gpa': gpa_val};
+    return map;
+}
+
+function standard_4_2() {
+    var threshold = new Array(85,75,60);
+    var letter_val = new Array('A', 'B', 'C');
+    var gpa_val = new Array(4.0,3.0,2.0);
+    var map = {'threshold': threshold, 'letter': letter_val, 'gpa': gpa_val};
+    return map;
+}
+
+function peking_4() {
+    var threshold = new Array(90,85,82,78,75,72,68,64,60);
+    var letter_val = new Array('A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D');
+    var gpa_val = new Array(4.0,3.7,3.3,3.0,2.7,2.3,2.0,1.7,1.0);
+    var map = {'threshold': threshold, 'letter': letter_val, 'gpa': gpa_val};
+    return map;
+}
+
+function canada_4_3() {
+    var threshold = new Array(90,85,80,75,70,65,60);
+    var letter_val = new Array('A+', 'A', 'A-', 'B+', 'B', 'C+', 'C');
+    var gpa_val = new Array(4.3, 4.0,3.7,3.3,3.0,2.7,2.3);
+    var map = {'threshold': threshold, 'letter': letter_val, 'gpa': gpa_val};
+    return map;
+}
+
+function CSU_4_3() {
+    var threshold = new Array(95,90,85,82,78,75,72,68,65,64,61,60);
+    var letter_val = new Array('A+','A','A-','B+','B','B-','C+','C','C-','D+','D','D-');
+    var gpa_val = new Array(4.3,4.0,3.7,3.3,3.0,2.7,2.3,2.0,1.7,1.5,1.3,1.0);
+    var map = {'threshold': threshold, 'letter': letter_val, 'gpa': gpa_val};
+    return map;
+}
+
+function SJTU_4_3() {
+    var threshold = new Array(95,90,85,80,75,70,67,65,62,60);
+    var letter_val = new Array('A+','A','B+','B','C+','C','C-','D+','D','D-');
+    var gpa_val = new Array(4.3,4.0,3.7,3.3,3.0,2.7,2.3,2.0,1.7,1.0);
+    var map = {'threshold': threshold, 'letter': letter_val, 'gpa': gpa_val};
+    return map;
 }
